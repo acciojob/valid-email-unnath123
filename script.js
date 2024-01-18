075352f6955475ac9cddbe6275ccdaadaeb08f6f
unnath123 <64188873+unnath123@users.noreply.github.com>
2024-01-18 20:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function validEmail(str) {
   //your JS code here.
-}
+
+  // return String(email)
+  //   .toLowerCase()
+  //   .match(
+  //     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  //   );
+	  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+	}
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
